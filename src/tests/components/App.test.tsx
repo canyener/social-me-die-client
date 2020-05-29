4abc6f4a31dc251e3fba1d25aec580bf1e8e0880
canyener <can.yener@hotmail.com.tr>
2020-05-30 01:40:37 +0300

    Adds test cases for state changes

--- src/tests/components/App.test.tsx
+++ src/tests/components/App.test.tsx
@@ -3,8 +3,34 @@ import { shallow } from 'enzyme'
 
 import App from '../../App'
 
-test('Should render App correctly', () => {
-  const wrapper = shallow(<App />)
+let wrapper: any
+
+beforeEach(() => {
+  wrapper = shallow(<App />)
+})
+
+test('Should render App correctly with default state', () => {
+  expect(wrapper).toMatchSnapshot()
+})
+
+test('Should render App correctly with values', () => {
+  wrapper.instance().setState(() => ({
+    values: [{
+      id: 1,
+      name: 'Value 1'
+    }, {
+      id: 2, 
+      value: 'Value 2'
+    }]
+  }))
 
   expect(wrapper).toMatchSnapshot()
 })
+
+test('Should set default state to empty array', () => {
+  const state = wrapper.instance().state
+  expect(state).toStrictEqual({
+    values: []
+  })
+})
+
