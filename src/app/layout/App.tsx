25e35400f4cf3e0c413616f78ae908e274f487ee
canyener <canyener1337@gmail.com>
2020-06-08 00:02:52 +0300

    Moves App component and styles.css under app/layout folder

---
+++ src/app/layout/App.tsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { Header, Icon, List } from 'semantic-ui-react'
+
+import axios from 'axios'
+
+class App extends Component {
+  state = {
+    values: []
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/values').then(response => {
+      this.setState(() => ({ values: response.data }))
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Header as='h2'>
+          <Icon name='users' />
+          <Header.Content>Social Me Die</Header.Content>
+        </Header>
+        <List>
+          {
+            this.state.values.map((value: any) => 
+              <List.Item key={value.id}>
+                {value.name}
+              </List.Item>)
+          }
+        </List>
+      </div>
+    )
+  }
+}
+
+export default App
