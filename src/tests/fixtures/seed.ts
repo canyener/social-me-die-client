d29783d01a541cb316483e35740ff82e46134e7a
canyener <canyener1337@gmail.com>
2020-06-08 10:28:31 +0300

    Adds seed activities

--- src/tests/fixtures/seed.ts
+++ src/tests/fixtures/seed.ts
@@ -9,4 +9,22 @@ const fakeValues = [{
   name: 'Value 3'
 }]
 
-export { fakeValues }
+const fakeActivities = [{
+  id: '1',
+  title:'Fake Activity 1',
+  category: 'Fake Category 1',
+  city: 'Fake City',
+  date: '2020-01-01',
+  description: 'Fake description',
+  venue: 'Fake Venue'
+},{
+  id: '2',
+  title:'Fake Activity 2',
+  category: 'Fake Category 2',
+  city: 'Fake City 2',
+  date: '2020-01-01',
+  description: 'Fake description 2',
+  venue: 'Fake Venue 2'
+}]
+
+export { fakeValues, fakeActivities }
