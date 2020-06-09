e0632a5a4597898dae5215b868aa89bb9b27d76f
canyener <canyener1337@gmail.com>
2020-06-09 23:13:25 +0300

    Tweaks class component to functional component and implements hooks

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,45 +1,37 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Header, Icon, List } from 'semantic-ui-react'
 
 import axios from 'axios'
 
 import { IActivity } from '../models/activity'
 
-interface IState {
-  activities: IActivity[]
-}
+const App = () => {
 
-class App extends Component<{}, IState> {
-  readonly state : IState = {
-    activities: []
-  }
+  const [activities, setActivities] = useState<IActivity[]>([])
 
-  componentDidMount() {
+  useEffect(() => {
     axios.get<IActivity[]>('http://localhost:5000/api/activities').then(response => {
-      this.setState(() => ({ 
-        activities: response.data 
-      }))
+      setActivities(response.data)
     })
-  }
+  }, [])
 
-  render() {
-    return (
-      <div>
-        <Header as='h2'>
-          <Icon name='users' />
-          <Header.Content>Social Me Die</Header.Content>
-        </Header>
-        <List>
-          {
-            this.state.activities.map((activity) => 
-              <List.Item key={activity.id}>
-                {activity.title}
-              </List.Item>)
-          }
-        </List>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Header as='h2'>
+        <Icon name='users' />
+        <Header.Content>Social Me Die</Header.Content>
+      </Header>
+      <List>
+        {
+          activities.map((activity) =>
+            <List.Item key={activity.id}>
+              {activity.title}
+            </List.Item>)
+        }
+      </List>
+    </div>
+  )
+
 }
 
 export default App
