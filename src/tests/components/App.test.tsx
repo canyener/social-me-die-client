07ac9cdb12556a0d7e3bc22637ee1f2582ff87d3
canyener <canyener1337@gmail.com>
2020-06-09 23:13:59 +0300

    Tweaks tests

--- src/tests/components/App.test.tsx
+++ src/tests/components/App.test.tsx
@@ -1,18 +1,42 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
+import axios from 'axios'
+import { act } from 'react-dom/test-utils'
+
+import { fakeActivities } from '../fixtures/seed'
 
 import App from '../../app/layout/App'
 
-const flushPromises = () => new Promise(resolve => setImmediate(resolve))
+afterAll(() => {
+  jest.restoreAllMocks()
+})
 
-test('Should render App correctly', () => {
-  const wrapper = shallow(<App />)
+test('Should render App correctly when activities are fetched successfully', async () => {
+  const fakeAxiosResponse = { data: fakeActivities }
+  jest.spyOn(axios, 'get').mockResolvedValueOnce(fakeAxiosResponse)
+
+  const wrapper = mount(<App />)
+
+  await act(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 0));
+  })
+
+  wrapper.update()
+
   expect(wrapper).toMatchSnapshot()
 })
 
-test('Should render App correctly with data', async () => {
-  const wrapper = shallow(<App />)
-  await flushPromises()
+test('Should render App correctly with empty activity list', async () => {
+  const fakeAxiosResponse = { data: [] }
+  jest.spyOn(axios, 'get').mockResolvedValueOnce(fakeAxiosResponse)
+
+  const wrapper = mount(<App />)
+
+  await act(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 0));
+  })
+
   wrapper.update()
+
   expect(wrapper).toMatchSnapshot()
 })
