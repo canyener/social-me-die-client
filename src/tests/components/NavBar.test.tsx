573be87a7e7a107ff40054e7847f85ec8474488b
canyener <can.yener@hotmail.com.tr>
2020-06-14 18:49:17 +0300

    Adds snapshot test for NavBar component

---
+++ src/tests/components/NavBar.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import { NavBar } from '../../features/nav/NavBar'
+
+test('Should render NavBar correctly', () => {
+  const wrapper = shallow(<NavBar />)
+
+  expect(wrapper).toMatchSnapshot()
+})
