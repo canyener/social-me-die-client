84a3e1da7b314c80124687039769cb3fdace7de1
canyener <can.yener@hotmail.com.tr>
2020-06-14 21:26:52 +0300

    Renders ActivityDashboard inside App component

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect, Fragment } from 'react'
-import { List, Container } from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react'
 
 import axios from 'axios'
 
 import { IActivity } from '../models/activity'
 import { NavBar } from '../../features/nav/NavBar'
+import { ActivityDashboard } from '../../features/activities/dashboard/ActivityDashboard'
 
 const App = () => {
 
@@ -20,14 +21,7 @@ const App = () => {
     <Fragment>
       <NavBar />
       <Container style={{ marginTop: '7em' }}>
-        <List>
-          {
-            activities.map((activity) =>
-              <List.Item key={activity.id}>
-                {activity.title}
-              </List.Item>)
-          }
-        </List>
+        <ActivityDashboard activities={activities} />
       </Container>
     </Fragment>
   )
