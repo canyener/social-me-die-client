8d9d627555f24c638047e4d338bdb6752f6311be
canyener <canyener1337@gmail.com>
2020-06-25 11:35:11 +0300

    Tweaks activity date to locale string and US time zone

--- src/features/activities/dashboard/ActivityList.tsx
+++ src/features/activities/dashboard/ActivityList.tsx
@@ -15,7 +15,7 @@ export const ActivityList: React.FC<IProps> = ({ activities }) => (
           <Item key={activity.id}>
             <Item.Content>
               <Item.Header as='a'>{activity.title}</Item.Header>
-              <Item.Meta>{activity.date.toString()}</Item.Meta>
+              <Item.Meta>{activity.date.toLocaleString("en-US", {timeZone: "America/New_York"})}</Item.Meta>
               <Item.Description>
                 <div>{activity.description}</div>
                 <div>{activity.city}, {activity.venue}</div>
