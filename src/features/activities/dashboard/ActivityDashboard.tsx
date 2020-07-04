1fcbb53242c364112da15ce6642003ee258f59fb
canyener <can.yener@hotmail.com.tr>
2020-07-04 13:51:16 +0300

    Adds ActivityDetails to ActivityDashboard

--- src/features/activities/dashboard/ActivityDashboard.tsx
+++ src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,16 +3,20 @@ import { Grid } from 'semantic-ui-react'
 
 import { IActivity } from '../../../app/models/activity'
 import { ActivityList } from './ActivityList'
+import { ActivityDetails } from '../details/ActivityDetails'
 
 interface IProps {
   activities: IActivity[]
 }
 
-export const ActivityDashboard: React.FC<IProps> = ( { activities }) => {
+export const ActivityDashboard: React.FC<IProps> = ({ activities }) => {
   return (
     <Grid>
       <Grid.Column width={10}>
-        <ActivityList activities={activities} />      
+        <ActivityList activities={activities} />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <ActivityDetails />
       </Grid.Column>
     </Grid>
   )
