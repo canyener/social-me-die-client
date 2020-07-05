c904171864c71f931c5b89b46848c27a220db8b0
canyener <can.yener@hotmail.com.tr>
2020-07-05 16:43:33 +0300

    Implicitly returns jsx in ActivityDetails component

--- src/features/activities/details/ActivityDetails.tsx
+++ src/features/activities/details/ActivityDetails.tsx
@@ -1,25 +1,23 @@
 import React from 'react'
 import { Card, Image, Button } from 'semantic-ui-react'
 
-export const ActivityDetails = () => {
-  return (
-    <Card fluid>
-      <Image src='/assets/placeholder.png' wrapped ui={false} />
-      <Card.Content>
-        <Card.Header>Title</Card.Header>
-        <Card.Meta>
-          <span>Date</span>
-        </Card.Meta>
-        <Card.Description>
-          Description
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <Button.Group widths={2}>
-          <Button basic color='blue'>Edit</Button>
-          <Button basic color='grey'>Cancel</Button>
-        </Button.Group>
-      </Card.Content>
-    </Card>
-  )
-}
+export const ActivityDetails = () => (
+  <Card fluid>
+    <Image src='/assets/placeholder.png' wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>Title</Card.Header>
+      <Card.Meta>
+        <span>Date</span>
+      </Card.Meta>
+      <Card.Description>
+        Description
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <Button.Group widths={2}>
+        <Button basic color='blue'>Edit</Button>
+        <Button basic color='grey'>Cancel</Button>
+      </Button.Group>
+    </Card.Content>
+  </Card>
+)
