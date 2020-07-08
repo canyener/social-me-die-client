186a406d0144720175c8e312c89e0235e7cb5a77
canyener <can.yener@hotmail.com.tr>
2020-07-09 00:23:24 +0300

    Adds selectActivity function signature to IProps and implements it to the component

--- src/features/activities/dashboard/ActivityList.tsx
+++ src/features/activities/dashboard/ActivityList.tsx
@@ -5,9 +5,10 @@ import { IActivity } from '../../../app/models/activity'
 
 interface IProps {
   activities: IActivity[]
+  selectActivity: (id: string) => void
 }
 
-export const ActivityList: React.FC<IProps> = ({ activities }) => (
+export const ActivityList: React.FC<IProps> = ({ activities, selectActivity }) => (
   <Segment clearing>
     <Item.Group divided>
       {
@@ -15,13 +16,18 @@ export const ActivityList: React.FC<IProps> = ({ activities }) => (
           <Item key={activity.id}>
             <Item.Content>
               <Item.Header as='a'>{activity.title}</Item.Header>
-              <Item.Meta>{activity.date.toLocaleString("en-US", {timeZone: "America/New_York"})}</Item.Meta>
+              <Item.Meta>{activity.date.toLocaleString("en-US", { timeZone: "America/New_York" })}</Item.Meta>
               <Item.Description>
                 <div>{activity.description}</div>
                 <div>{activity.city}, {activity.venue}</div>
               </Item.Description>
               <Item.Extra>
-                <Button floated='right' content='View' color='blue' />
+                <Button
+                  onClick={() => selectActivity(activity.id)}
+                  floated='right'
+                  content='View'
+                  color='blue'
+                />
                 <Label basic content={activity.category} />
               </Item.Extra>
             </Item.Content>
