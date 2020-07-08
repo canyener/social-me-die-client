875032d5682e3c302811b127ffbdb29328cdcb3b
canyener <can.yener@hotmail.com.tr>
2020-07-09 00:26:33 +0300

    Adds missing props to wrapper, adds test case for selectActivity function

--- src/tests/components/ActivityList.test.tsx
+++ src/tests/components/ActivityList.test.tsx
@@ -5,7 +5,16 @@ import { ActivityList } from '../../features/activities/dashboard/ActivityList'
 import { fakeActivities } from '../fixtures/seed'
 
 test('Should render ActivityList correctly', () => {
-  const wrapper = shallow(<ActivityList activities={fakeActivities} />)
+  const wrapper = shallow(<ActivityList activities={fakeActivities} selectActivity={() => {}} />)
 
   expect(wrapper).toMatchSnapshot()
 })
+
+test('Should call selectActivity once when button is clicked', () => {
+  const selectActivitySpy = jest.fn()
+
+  const wrapper = shallow(<ActivityList activities={fakeActivities} selectActivity={selectActivitySpy} />)
+  wrapper.find('Button').first().simulate('click')
+  expect(selectActivitySpy).toHaveBeenCalledTimes(1)
+})
+
