4c403a0e516bcdf845eaf63526b56eb917b72c6c
canyener <can.yener@hotmail.com.tr>
2020-07-09 00:25:48 +0300

    Adds activity prop to wrapper

--- src/tests/components/ActivityDetails.test.tsx
+++ src/tests/components/ActivityDetails.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+
 import { ActivityDetails } from '../../features/activities/details/ActivityDetails'
+import { fakeActivities } from '../fixtures/seed'
 
 test('Should render ActivityDetails correctly', () => {
-  const wrapper = shallow(<ActivityDetails />)
+  const wrapper = shallow(<ActivityDetails activity={fakeActivities[0]} />)
   
   expect(wrapper).toMatchSnapshot()
 })
