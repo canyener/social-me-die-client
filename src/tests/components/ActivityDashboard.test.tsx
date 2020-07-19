ad9bc1e175e6e318220772561561072d80af7832
canyener <can.yener@hotmail.com.tr>
2020-07-19 22:00:02 +0300

    Tweaks snapshot tests to be rendered with editMode

--- src/tests/components/ActivityDashboard.test.tsx
+++ src/tests/components/ActivityDashboard.test.tsx
@@ -4,8 +4,26 @@ import { shallow } from 'enzyme'
 import { ActivityDashboard } from '../../features/activities/dashboard/ActivityDashboard'
 import { fakeActivities } from '../fixtures/seed'
 
-test('Should render ActivityDashboard correctly', () => {
-  const wrapper = shallow(<ActivityDashboard activities={fakeActivities} selectedActivity={fakeActivities[0]} selectActivity={() => {}}  />)
+test('Should render ActivityDashboard correctly with editMode equals to false', () => {
+  const wrapper = shallow(
+    <ActivityDashboard 
+      activities={fakeActivities} 
+      selectedActivity={fakeActivities[0]} 
+      selectActivity={() => {}}
+      editMode={false}
+      setEditMode={() => {}}  />)
+
+  expect(wrapper).toMatchSnapshot()
+})
+
+test('Should render ActivityDashboard correctly with editMode equals to true', () => {
+  const wrapper = shallow(
+    <ActivityDashboard 
+      activities={fakeActivities} 
+      selectedActivity={fakeActivities[0]} 
+      selectActivity={() => {}}
+      editMode={true}
+      setEditMode={() => {}}  />)
 
   expect(wrapper).toMatchSnapshot()
 })
