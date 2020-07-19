a32e3e1e86b09116825538457926642530719341
canyener <can.yener@hotmail.com.tr>
2020-07-19 22:00:28 +0300

    Adds a test case that tests setEditMode function

--- src/tests/components/ActivityDetails.test.tsx
+++ src/tests/components/ActivityDetails.test.tsx
@@ -5,7 +5,16 @@ import { ActivityDetails } from '../../features/activities/details/ActivityDetai
 import { fakeActivities } from '../fixtures/seed'
 
 test('Should render ActivityDetails correctly', () => {
-  const wrapper = shallow(<ActivityDetails activity={fakeActivities[0]} />)
+  const wrapper = shallow(<ActivityDetails activity={fakeActivities[0]} setEditMode={() => {}} />)
   
   expect(wrapper).toMatchSnapshot()
+})
+
+test('Should call setEditMode with true when Edit button is clicked', () => {
+
+  const setEditModeSpy = jest.fn()
+  
+  const wrapper = shallow(<ActivityDetails activity={fakeActivities[0]} setEditMode={setEditModeSpy} />)
+  wrapper.find('Button').first().simulate('click')
+  expect(setEditModeSpy).toHaveBeenCalledWith(true)
 })
