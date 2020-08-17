84c968899034c1ec72e931d0c70dc7040baf1e82
canyener <can.yener@hotmail.com.tr>
2020-08-17 22:11:58 +0300

    Updates wrapper and adds test case for Cancel button click

--- src/tests/components/ActivityForm.test.tsx
+++ src/tests/components/ActivityForm.test.tsx
@@ -4,7 +4,17 @@ import { shallow } from 'enzyme'
 import { ActivityForm } from '../../features/activities/form/ActivityForm'
 
 test('Should render ActivityForm correctly', () => {
-  const wrapper = shallow(<ActivityForm />)
+  const wrapper = shallow(<ActivityForm setEditMode={() => {}} />)
 
   expect(wrapper).toMatchSnapshot()
+})
+
+test('Should call setEditMode with false when Cancel button is clicked ', () => {
+  const setEditModeSpy = jest.fn()
+
+  const wrapper = shallow(<ActivityForm setEditMode={setEditModeSpy} />)
+
+  wrapper.find('Button').at(1).simulate('click')
+
+  expect(setEditModeSpy).toHaveBeenCalledWith(false)
 })
