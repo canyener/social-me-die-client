9a826fa67000f87b55c658b40e2fdd07db8dadee
Can Yener <canyener1337@gmail.com>
2020-09-30 22:01:05 +0300

    Adds initial form state

--- src/features/activities/form/ActivityForm.tsx
+++ src/features/activities/form/ActivityForm.tsx
@@ -1,21 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Segment, Form, Button } from 'semantic-ui-react'
+import { IActivity } from '../../../app/models/activity'
 
 interface IProps {
   setEditMode: (editMode: boolean) => void
+  activity: IActivity | null
 }
 
-export const ActivityForm: React.FC<IProps> = ({ setEditMode }) => (
-  <Segment clearing>
-    <Form>
-      <Form.Input placeholder='Title' />
-      <Form.TextArea rows={2} placeholder='Description' />
-      <Form.Input placeholder='Category' />
-      <Form.Input type='date' placeholder='Date' />
-      <Form.Input placeholder='City' />
-      <Form.Input placeholder='Venue' />
-      <Button floated='right' positive type='submit' content='Submit' />
-      <Button onClick={() => setEditMode(false)} floated='right' type='button' content='Cancel' />
-    </Form>
-  </Segment>
-)
+export const ActivityForm: React.FC<IProps> = ({ setEditMode, activity: initialFormState }) => {
+  const initializeForm = () => {
+    if (initialFormState) {
+      return initialFormState
+    } else {
+      return {
+        id: '',
+        title: '',
+        description: '',
+        category: '',
+        city: '',
+        venue: '',
+        date: ''
+      }
+    }
+  }
+
+  const [activity, setActivity] = useState<IActivity>(initializeForm)
+
+  return (
+    <Segment clearing>
+      <Form>
+        <Form.Input placeholder='Title' value={activity.title} />
+        <Form.TextArea rows={2} placeholder='Description' value={activity.description} />
+        <Form.Input placeholder='Category' value={activity.category} />
+        <Form.Input type='date' placeholder='Date' value={activity.date} />
+        <Form.Input placeholder='City' value={activity.city} />
+        <Form.Input placeholder='Venue' value={activity.venue} />
+        <Button floated='right' positive type='submit' content='Submit' />
+        <Button onClick={() => setEditMode(false)} floated='right' type='button' content='Cancel' />
+      </Form>
+    </Segment>
+  )
+}
