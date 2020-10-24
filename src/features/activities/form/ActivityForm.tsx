cc19d2cec3b47238f2d408029cf3ad79e78742a1
canyener <canyener1337@gmail.com>
2020-10-24 19:03:29 +0300

    Handles create and edit activity on form submit

--- src/features/activities/form/ActivityForm.tsx
+++ src/features/activities/form/ActivityForm.tsx
@@ -1,13 +1,21 @@
 import React, { FormEvent, useState } from 'react'
 import { Segment, Form, Button } from 'semantic-ui-react'
 import { IActivity } from '../../../app/models/activity'
+import { v4 as uuid } from 'uuid'
 
 interface IProps {
   setEditMode: (editMode: boolean) => void
-  activity: IActivity | null
+  activity: IActivity | null,
+  createActivity: (activity: IActivity) => void
+  editActivity: (activity: IActivity) => void
 }
 
-export const ActivityForm: React.FC<IProps> = ({ setEditMode, activity: initialFormState }) => {
+export const ActivityForm: React.FC<IProps> = ({
+  setEditMode,
+  activity: initialFormState,
+  createActivity,
+  editActivity
+}) => {
   const initializeForm = () => {
     if (initialFormState) {
       return initialFormState
@@ -26,6 +34,18 @@ export const ActivityForm: React.FC<IProps> = ({ setEditMode, activity: initialF
 
   const [activity, setActivity] = useState<IActivity>(initializeForm)
 
+  const handleSubmit = () => {
+    if (activity.id.length === 0) {
+      let newActivity = {
+        ...activity,
+        id: uuid()
+      }
+      createActivity(newActivity)
+    } else {
+      editActivity(activity)
+    }
+  }
+
   const handleInputChange = (event: FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = event.currentTarget
     setActivity({ ...activity, [name]: value })
@@ -33,7 +53,7 @@ export const ActivityForm: React.FC<IProps> = ({ setEditMode, activity: initialF
 
   return (
     <Segment clearing>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Input
           onChange={handleInputChange}
           name='title'
