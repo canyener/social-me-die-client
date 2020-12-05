c7fa158ac4553217285bb2bd408a14eb85574909
canyener <canyener1337@gmail.com>
2020-12-06 00:24:11 +0300

    Adds test case for delete activity

--- src/tests/components/ActivityList.test.tsx
+++ src/tests/components/ActivityList.test.tsx
@@ -5,7 +5,12 @@ import { ActivityList } from '../../features/activities/dashboard/ActivityList'
 import { fakeActivities } from '../fixtures/seed'
 
 test('Should render ActivityList correctly', () => {
-  const wrapper = shallow(<ActivityList activities={fakeActivities} selectActivity={() => {}} />)
+  const wrapper = shallow(
+    <ActivityList
+      activities={fakeActivities}
+      selectActivity={() => { }}
+      deleteActivity={() => { }}
+    />)
 
   expect(wrapper).toMatchSnapshot()
 })
@@ -13,7 +18,25 @@ test('Should render ActivityList correctly', () => {
 test('Should call selectActivity once when button is clicked', () => {
   const selectActivitySpy = jest.fn()
 
-  const wrapper = shallow(<ActivityList activities={fakeActivities} selectActivity={selectActivitySpy} />)
+  const wrapper = shallow(
+    <ActivityList
+      activities={fakeActivities}
+      selectActivity={selectActivitySpy}
+      deleteActivity={() => { }}
+    />)
   wrapper.find('Button').first().simulate('click')
   expect(selectActivitySpy).toHaveBeenCalledTimes(1)
+})
+
+test('Should call deleteActivity once when delete button is clicked', () => {
+  const deleteActivitySpy = jest.fn()
+  const wrapper = shallow(
+    <ActivityList
+      activities={fakeActivities}
+      selectActivity={() => { }}
+      deleteActivity={deleteActivitySpy}
+    />)
+
+    wrapper.find('Button').at(1).simulate('click')
+    expect(deleteActivitySpy).toHaveBeenCalledTimes(1)
 })
