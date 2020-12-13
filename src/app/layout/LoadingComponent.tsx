cf4323df018827b311c9d802e68bdcb0fa077b41
canyener <canyener1337@gmail.com>
2020-12-13 16:42:02 +0300

    Sets default inverted value as true

--- src/app/layout/LoadingComponent.tsx
+++ src/app/layout/LoadingComponent.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Dimmer, Loader } from 'semantic-ui-react'
 
-const LoadingComponent: React.FC<{ inverted?: boolean, content?: string }> = ({ inverted, content }) => {
+const LoadingComponent: React.FC<{ inverted?: boolean, content?: string }> = ({
+  inverted = true,
+  content
+}) => {
   return (
     <Dimmer active inverted={inverted}>
       <Loader content={content} />
