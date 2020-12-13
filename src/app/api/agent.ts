0ed9859b9f7cbcefd00e2fe3d2c6994243b2f0f1
canyener <canyener1337@gmail.com>
2020-12-13 15:44:35 +0300

    Adds 1 second delay to all requests

--- src/app/api/agent.ts
+++ src/app/api/agent.ts
@@ -5,11 +5,14 @@ axios.defaults.baseURL = 'http://localhost:5000/api'
 
 const responseBody = (response: AxiosResponse) => response.data
 
+const sleep = (ms: number) => (response: AxiosResponse) =>
+   new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms))
+
 const requests = {
-  get: (url: string) => axios.get(url).then(responseBody),
-  post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
-  put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
-  delete: (url:string) => axios.delete(url).then(responseBody)
+  get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
+  post: (url: string, body: {}) => axios.post(url, body).then(sleep(1000)).then(responseBody),
+  put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(responseBody),
+  delete: (url:string) => axios.delete(url).then(sleep(1000)).then(responseBody)
 }
 
 const Activities = {
