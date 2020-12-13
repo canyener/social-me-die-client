a494b69e4f43afe0831a8b5a5fdd346efb7416af
canyener <canyener1337@gmail.com>
2020-12-13 16:41:18 +0300

    Adds default loading state hook and uses it

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -6,12 +6,14 @@ import { NavBar } from '../../features/nav/NavBar'
 import { ActivityDashboard } from '../../features/activities/dashboard/ActivityDashboard'
 
 import agent from '../api/agent'
+import LoadingComponent from './LoadingComponent'
 
 const App = () => {
 
   const [activities, setActivities] = useState<IActivity[]>([])
   const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null)
   const [editMode, setEditMode] = useState(false)
+  const [loading, setLoading] = useState(true)
 
   const handleSelectActivity = (id: string) => {
     setSelectedActivity(activities.filter(activity => activity.id === id)[0])
@@ -57,7 +59,10 @@ const App = () => {
 
         setActivities(activities)
       })
+      .then(() => setLoading(false))
   }, [])
+
+  if(loading) return <LoadingComponent content="Loading Activities..."/>
 
   return (
     <Fragment>
