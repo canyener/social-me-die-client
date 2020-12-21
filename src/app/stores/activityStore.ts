b819f6c12f44e8b8261913519f67317c7266143f
canyener <can.yener@hotmail.com.tr>
2020-12-22 01:23:06 +0300

    Adds activityStore

---
+++ src/app/stores/activityStore.ts
@@ -0,0 +1,30 @@
+import { makeObservable, observable, action } from "mobx"
+import { createContext } from 'react'
+
+import { IActivity } from "../models/activity"
+import { Activities } from '../api/agent'
+
+class ActivityStore {
+  constructor() {
+    makeObservable(this)
+  }
+
+  @observable activities: IActivity[] = []
+  @observable loadingInitial = false
+
+  @action loadActivities = () => {
+    this.loadingInitial = true
+
+    Activities.list()
+      .then(activityList => {
+
+        activityList.forEach((activity) => {
+          activity.date = activity.date.split('.')[0]
+          this.activities.push(activity)
+        })
+      })
+      .finally(() => this.loadingInitial = false)
+  }
+}
+
+export default createContext(new ActivityStore())
