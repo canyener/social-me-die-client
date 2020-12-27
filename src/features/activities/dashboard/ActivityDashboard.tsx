f881ac9697cd46c74f56553525f0711300b97045
canyener <canyener1337@gmail.com>
2020-12-28 02:31:23 +0300

    Removes props that are moved to activity store

--- src/features/activities/dashboard/ActivityDashboard.tsx
+++ src/features/activities/dashboard/ActivityDashboard.tsx
@@ -11,10 +11,8 @@ import { ActivityForm } from '../form/ActivityForm'
 
 interface IProps {
   activities: IActivity[]
-  selectActivity: (id: string) => void
   setEditMode: (editMode: boolean) => void
   setSelectedActivity: (activity: IActivity | null) => void
-  createActivity: (activity: IActivity) => void
   editActivity: (activity: IActivity) => void
   deleteActivity: (event: SyntheticEvent<HTMLButtonElement>, id: string) => void
   submitting: boolean,
@@ -23,10 +21,8 @@ interface IProps {
 
 const ActivityDashboard: React.FC<IProps> = ({
   activities,
-  selectActivity,
   setEditMode,
   setSelectedActivity,
-  createActivity,
   editActivity,
   deleteActivity,
   submitting,
@@ -59,7 +55,6 @@ const ActivityDashboard: React.FC<IProps> = ({
             key={(selectedActivity && selectedActivity.id) || 0}
             setEditMode={setEditMode}
             activity={selectedActivity}
-            createActivity={createActivity}
             editActivity={editActivity}
             submitting={submitting}
           />
