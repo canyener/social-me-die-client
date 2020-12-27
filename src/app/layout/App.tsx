3efa41c5fba8dff798c1d383f2cac5da61aea3b5
canyener <can.yener@hotmail.com.tr>
2020-12-27 19:56:16 +0300

    Removes unnecessary props from ActivityDashboard

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -73,8 +73,6 @@ const App = () => {
         <ActivityDashboard
           activities={activityStore.activities}
           selectActivity={handleSelectActivity}
-          selectedActivity={selectedActivity}
-          editMode={editMode}
           setEditMode={setEditMode}
           setSelectedActivity={setSelectedActivity}
           createActivity={handleCreateActivity}
