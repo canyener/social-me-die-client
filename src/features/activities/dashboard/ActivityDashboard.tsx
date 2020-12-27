7c3e72077f1e4e73dce6632f87510c385dfa75a0
canyener <can.yener@hotmail.com.tr>
2020-12-27 19:57:29 +0300

    Implements activityStore and removes unnecessary props

--- src/features/activities/dashboard/ActivityDashboard.tsx
+++ src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,18 +1,17 @@
-import React, { SyntheticEvent } from 'react'
+import React, { SyntheticEvent, useContext } from 'react'
 import { Grid } from 'semantic-ui-react'
 import { observer } from 'mobx-react-lite'
 
 import { IActivity } from '../../../app/models/activity'
+import ActivityStore from '../../../app/stores/activityStore'
 
 import ActivityList from './ActivityList'
-import { ActivityDetails } from '../details/ActivityDetails'
+import ActivityDetails from '../details/ActivityDetails'
 import { ActivityForm } from '../form/ActivityForm'
 
 interface IProps {
   activities: IActivity[]
-  selectedActivity: IActivity | null
   selectActivity: (id: string) => void
-  editMode: boolean
   setEditMode: (editMode: boolean) => void
   setSelectedActivity: (activity: IActivity | null) => void
   createActivity: (activity: IActivity) => void
@@ -24,9 +23,7 @@ interface IProps {
 
 const ActivityDashboard: React.FC<IProps> = ({
   activities,
-  selectedActivity,
   selectActivity,
-  editMode,
   setEditMode,
   setSelectedActivity,
   createActivity,
@@ -34,42 +31,43 @@ const ActivityDashboard: React.FC<IProps> = ({
   deleteActivity,
   submitting,
   target
-}) => (
-  <Grid>
-    <Grid.Column width={10}>
-      <ActivityList
-        activities={activities}
-        selectActivity={selectActivity}
-        deleteActivity={deleteActivity}
-        submitting={submitting}
-        target={target}
-      />
-    </Grid.Column>
-    <Grid.Column width={6}>
-      {
-        selectedActivity
-        && !editMode
-        && (
-          <ActivityDetails
-            activity={selectedActivity}
-            setEditMode={setEditMode}
-            setSelectedActivity={setSelectedActivity}
-          />
-        )
-      }
-      {editMode && (
-        <ActivityForm
-          key={(selectedActivity && selectedActivity.id) || 0}
-          setEditMode={setEditMode}
-          activity={selectedActivity}
-          createActivity={createActivity}
-          editActivity={editActivity}
+}) => {
+  const activityStore = useContext(ActivityStore)
+  const { editMode, selectedActivity } = activityStore
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <ActivityList
+          deleteActivity={deleteActivity}
           submitting={submitting}
+          target={target}
         />
-      )}
-    </Grid.Column>
-  </Grid>
-)
+      </Grid.Column>
+      <Grid.Column width={6}>
+        {
+          selectedActivity
+          && !editMode
+          && (
+            <ActivityDetails
+              setEditMode={setEditMode}
+              setSelectedActivity={setSelectedActivity}
+            />
+          )
+        }
+        {editMode && (
+          <ActivityForm
+            key={(selectedActivity && selectedActivity.id) || 0}
+            setEditMode={setEditMode}
+            activity={selectedActivity}
+            createActivity={createActivity}
+            editActivity={editActivity}
+            submitting={submitting}
+          />
+        )}
+      </Grid.Column>
+    </Grid>
+  )
+}
 
 ActivityDashboard.displayName = 'ActivityDashboard'
 
--- src/features/activities/dashboard/ActivityList.tsx
+++ src/features/activities/dashboard/ActivityList.tsx
@@ -1,60 +1,62 @@
 import { observer } from 'mobx-react-lite'
-import React, { SyntheticEvent } from 'react'
+import React, { SyntheticEvent, useContext } from 'react'
 import { Item, Button, Label, Segment } from 'semantic-ui-react'
+
+import ActivityStore from '../../../app/stores/activityStore'
 
 import { IActivity } from '../../../app/models/activity'
 
 interface IProps {
-  activities: IActivity[]
-  selectActivity: (id: string) => void
-  deleteActivity: (event: SyntheticEvent<HTMLButtonElement>,id: string) => void
+  deleteActivity: (event: SyntheticEvent<HTMLButtonElement>, id: string) => void
   submitting: boolean
   target: string
 }
 
 const ActivityList: React.FC<IProps> = ({
-  activities,
-  selectActivity,
   deleteActivity,
   submitting,
   target
-}) => (
-  <Segment clearing>
-    <Item.Group divided>
-      {
-        activities.map(activity =>
-          <Item key={activity.id}>
-            <Item.Content>
-              <Item.Header as='a'>{activity.title}</Item.Header>
-              <Item.Meta>{activity.date}</Item.Meta>
-              <Item.Description>
-                <div>{activity.description}</div>
-                <div>{activity.city}, {activity.venue}</div>
-              </Item.Description>
-              <Item.Extra>
-                <Button
-                  onClick={() => selectActivity(activity.id)}
-                  floated='right'
-                  content='View'
-                  color='blue'
-                />
-                <Button
-                  name={activity.id}
-                  loading={target === activity.id && submitting}
-                  onClick={(e) => deleteActivity(e, activity.id)}
-                  floated='right'
-                  content='Delete'
-                  color='red'
-                />
-                <Label basic content={activity.category} />
-              </Item.Extra>
-            </Item.Content>
-          </Item>
-        )
-      }
-    </Item.Group>
-  </Segment>
-)
+}) => {
+  const activityStore = useContext(ActivityStore)
+  const { activities, selectActivity } = activityStore
+  return (
+    <Segment clearing>
+      <Item.Group divided>
+        {
+          activities.map(activity =>
+            <Item key={activity.id}>
+              <Item.Content>
+                <Item.Header as='a'>{activity.title}</Item.Header>
+                <Item.Meta>{activity.date}</Item.Meta>
+                <Item.Description>
+                  <div>{activity.description}</div>
+                  <div>{activity.city}, {activity.venue}</div>
+                </Item.Description>
+                <Item.Extra>
+                  <Button
+                    onClick={() => selectActivity(activity.id)}
+                    floated='right'
+                    content='View'
+                    color='blue'
+                  />
+                  <Button
+                    name={activity.id}
+                    loading={target === activity.id && submitting}
+                    onClick={(e) => deleteActivity(e, activity.id)}
+                    floated='right'
+                    content='Delete'
+                    color='red'
+                  />
+                  <Label basic content={activity.category} />
+                </Item.Extra>
+              </Item.Content>
+            </Item>
+          )
+        }
+      </Item.Group>
+    </Segment>
+  )
+}
 
 ActivityList.displayName = 'ActivityList'
 
