5443ef355b44b9d7aa9374771be176160a1b5a41
canyener <can.yener@hotmail.com.tr>
2021-01-02 01:41:49 +0300

    Adds routes

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect, Fragment, useContext } from 'react'
 import { Container } from 'semantic-ui-react'
 import { observer } from 'mobx-react-lite'
+import { Route } from 'react-router-dom'
 
 import NavBar from '../../features/nav/NavBar'
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard'
 import LoadingComponent from './LoadingComponent'
 
 import ActivityStore from '../stores/activityStore'
+import HomePage from '../../features/home/HomePage'
+import ActivityForm from '../../features/activities/form/ActivityForm'
 
 const App = () => {
 
@@ -22,7 +25,9 @@ const App = () => {
     <Fragment>
       <NavBar />
       <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard />
+        <Route exact path='/' component={HomePage} />
+        <Route path='/activities' component={ActivityDashboard} />
+        <Route path='/createActivity' component={ActivityForm} />
       </Container>
     </Fragment>
   )
