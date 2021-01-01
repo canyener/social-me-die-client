741c2824b5a6b34713d75d013a9a2a5cd0c78c43
canyener <can.yener@hotmail.com.tr>
2021-01-02 01:43:51 +0300

    Adds Homepage component

---
+++ src/features/home/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'semantic-ui-react'
+
+const HomePage = () => {
+  return (
+    <Container style={{ marginTop: '7em' }}>
+      <h1>Home page</h1>
+    </Container>
+  )
+}
+
+export default HomePage
