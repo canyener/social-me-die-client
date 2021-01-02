a952628bbfbc1f9443ce5cb0fa6c9090b0acbbe8
canyener <can.yener@hotmail.com.tr>
2021-01-02 22:35:24 +0300

    REmoves ActivityDetails from ActivityDashboard

--- src/features/activities/dashboard/ActivityDashboard.tsx
+++ src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,35 +1,17 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { Grid } from 'semantic-ui-react'
 import { observer } from 'mobx-react-lite'
 
-import ActivityStore from '../../../app/stores/activityStore'
-
 import ActivityList from './ActivityList'
-import ActivityDetails from '../details/ActivityDetails'
-import ActivityForm from '../form/ActivityForm'
 
 const ActivityDashboard: React.FC = () => {
-  const activityStore = useContext(ActivityStore)
-  const { editMode, selectedActivity } = activityStore
   return (
     <Grid>
       <Grid.Column width={10}>
         <ActivityList />
       </Grid.Column>
       <Grid.Column width={6}>
-        {
-          selectedActivity
-          && !editMode
-          && (
-            <ActivityDetails />
-          )
-        }
-        {editMode && (
-          <ActivityForm
-            key={(selectedActivity && selectedActivity.id) || 0}
-            activity={selectedActivity}
-          />
-        )}
+        <h2>Activity Filters</h2>
       </Grid.Column>
     </Grid>
   )
