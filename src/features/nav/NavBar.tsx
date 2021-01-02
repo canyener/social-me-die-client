0c86fe2cd4c23d085aed78c498dfdd9bba325292
canyener <can.yener@hotmail.com.tr>
2021-01-02 21:28:54 +0300

    Adds NavLinks, removes store implementation and mofifies tests

--- src/features/nav/NavBar.tsx
+++ src/features/nav/NavBar.tsx
@@ -1,21 +1,18 @@
-import React, { useContext } from 'react'
-import { observer } from 'mobx-react-lite'
+import React from 'react'
 import { Menu, Container, Button } from 'semantic-ui-react'
-
-import ActivityStore from '../../app/stores/activityStore'
+import { NavLink } from 'react-router-dom'
 
 const NavBar: React.FC = () => {
-  const activityStore = useContext(ActivityStore)
   return (
   <Menu fixed='top' inverted>
     <Container>
-      <Menu.Item header>
+      <Menu.Item header as={NavLink} exact to='/'>
         <img src="/assets/logo.png" alt="logo" style={{ marginRight: '10px' }} />
           Social Me Die
       </Menu.Item>
-      <Menu.Item name='Activities' />
+      <Menu.Item name='Activities' as={NavLink} to='/activities' />
       <Menu.Item>
-        <Button onClick={activityStore.openCreateForm} positive content='Create Activity' />
+        <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
       </Menu.Item>
     </Container>
   </Menu>
@@ -23,4 +20,4 @@ const NavBar: React.FC = () => {
 
 NavBar.displayName = 'NavBar'
 
-export default observer(NavBar)
+export default NavBar
--- src/tests/components/NavBar.test.tsx
+++ src/tests/components/NavBar.test.tsx
@@ -1,27 +1,9 @@
 import React from 'react'
-import { shallow, mount } from 'enzyme'
+import { shallow } from 'enzyme'
 
 import NavBar from '../../features/nav/NavBar'
 
 test('Should render NavBar correctly', () => {
   const wrapper = shallow(<NavBar />)
   expect(wrapper).toMatchSnapshot()
-})
-
-test('Should call handler for openCreateForm when Create Activity button is clicked', () => {
-
-  const message = 'Will be refactored after mobx store implementation'
-  expect('Will be refactored after mobx store implementation').toBe(message)
-
-  // const context = {
-  //   openCreateForm: jest.fn()
-  // }
-  // const wrapper = mount(<NavBar />, { context })
-  // wrapper.setContext(context)
-  // // const wrapper = shallow(<NavBar />)
-  // // wrapper.setState({
-  // //   openCreateForm: jest.fn()
-  // // })
-  // wrapper.find('Button').first().simulate('click')
-  // expect(context.openCreateForm).toHaveBeenCalledTimes(1)
 })
