c900f08b6867b448de4c56bb9ce51c0aa5da11f4
canyener <canyener1337@gmail.com>
2021-01-11 00:05:15 +0300

    Adds base layout style for ActivityListItem

--- src/features/activities/dashboard/ActivityListItem.tsx
+++ src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,31 +1,43 @@
 import React from 'react'
-import { Item, Button, Label } from 'semantic-ui-react'
+import { Item, Button, Segment, Icon } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
 import { IActivity } from '../../../app/models/activity'
 
 const ActivityListItem: React.FC<{ activity: IActivity }> = ({ activity }) => {
   return (
-    <Item key={activity.id}>
-      <Item.Content>
-        <Item.Header as='a'>{activity.title}</Item.Header>
-        <Item.Meta>{activity.date}</Item.Meta>
-        <Item.Description>
-          <div>{activity.description}</div>
-          <div>{activity.city}, {activity.venue}</div>
-        </Item.Description>
-        <Item.Extra>
-          <Button
-            as={Link}
-            to={`/activities/${activity.id}`}
-            floated='right'
-            content='View'
-            color='blue'
-          />
-          <Label basic content={activity.category} />
-        </Item.Extra>
-      </Item.Content>
-    </Item>
+    <Segment.Group>
+      <Segment>
+        <Item.Group>
+          <Item>
+            <Item.Image size='tiny' circular src='/assets/user.png' />
+            <Item.Content>
+              <Item.Header as='a'>{activity.title}</Item.Header>
+              <Item.Description>
+                Hosted by Cancan
+            </Item.Description>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </Segment>
+      <Segment>
+        <Icon name='clock' /> {activity.date}
+        <Icon name='marker' /> {activity.venue}, {activity.city}
+      </Segment>
+      <Segment secondary>
+        Attendees will go here
+      </Segment>
+      <Segment clearing>
+        <span>{activity.description}</span>
+        <Button
+          as={Link}
+          to={`/activities/${activity.id}`}
+          floated='right'
+          content='View'
+          color='blue'
+        />
+      </Segment>
+    </Segment.Group>
   )
 }
 
