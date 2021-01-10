6fbcbabff55896aa7ac8429061ac93d1726e4dcc
canyener <canyener1337@gmail.com>
2021-01-11 00:02:30 +0300

    Groups ActivityListItem's by date

--- src/features/activities/dashboard/ActivityList.tsx
+++ src/features/activities/dashboard/ActivityList.tsx
@@ -1,6 +1,6 @@
 import { observer } from 'mobx-react-lite'
-import React, { useContext } from 'react'
-import { Item, Segment } from 'semantic-ui-react'
+import React, { Fragment, useContext } from 'react'
+import { Item, Label } from 'semantic-ui-react'
 
 import ActivityStore from '../../../app/stores/activityStore'
 import ActivityListItem from './ActivityListItem'
@@ -9,15 +9,24 @@ const ActivityList: React.FC = () => {
   const activityStore = useContext(ActivityStore)
   const { activitiesByDate } = activityStore
   return (
-    <Segment clearing>
-      <Item.Group divided>
-        {
-          activitiesByDate.map(activity =>
-            <ActivityListItem key={activity.id} activity={activity} />
-          )
-        }
-      </Item.Group>
-    </Segment>
+    <Fragment>
+      {
+        activitiesByDate.map(([group, activities]) => (
+          <Fragment key={group}>
+            <Label size='large' color='blue'>
+              {group}
+            </Label>
+            <Item.Group divided>
+              {
+                activities.map(activity =>
+                  <ActivityListItem key={activity.id} activity={activity} />
+                )
+              }
+            </Item.Group>
+
+          </Fragment>
+        ))}
+    </Fragment>
   )
 }
 
