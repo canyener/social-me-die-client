b53a21cb665c35a44801eb645137de0f5dc4ca4d
canyener <canyener1337@gmail.com>
2021-02-08 23:30:15 +0300

    Adds snapshot test for ActivityDetailedInfo component

---
+++ src/tests/components/ActivityDetailedInfo.test.tsx
@@ -0,0 +1,9 @@
+import { shallow } from 'enzyme'
+import ActivityDetailedInfo from '../../features/activities/details/ActivityDetailedInfo'
+import { fakeActivities } from '../fixtures/seed'
+
+test('Should render ActivityDetailedInfo, correctly', () => {
+  const wrapper = shallow(<ActivityDetailedInfo activity={fakeActivities[0]} />)
+
+  expect(wrapper).toMatchSnapshot()
+})
