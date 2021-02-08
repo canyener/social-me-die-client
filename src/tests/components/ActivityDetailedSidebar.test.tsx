991803d4259251fd1e00d567accda1b24bca5cf3
canyener <canyener1337@gmail.com>
2021-02-08 23:30:34 +0300

    Adds snapshot test for ActivityDetailedSidebar component

---
+++ src/tests/components/ActivityDetailedSidebar.test.tsx
@@ -0,0 +1,8 @@
+import { shallow } from 'enzyme'
+import ActivityDetailedSidebar from '../../features/activities/details/ActivityDetailedSidebar'
+
+ test('Should render ActivityDetailedSidebar correctly', () => {
+   const wrapper = shallow(<ActivityDetailedSidebar />)
+
+   expect(wrapper).toMatchSnapshot()
+ })
