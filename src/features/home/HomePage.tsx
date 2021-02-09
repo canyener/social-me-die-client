83a6c82d9e0f925537ef6b974875b52e1fd15711
canyener <can.yener@hotmail.com.tr>
2021-02-09 09:46:23 +0300

    Gives a better look to Home page

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
-import { Container } from 'semantic-ui-react'
+import { Button, Container, Header, Segment, Image } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
-    <Container style={{ marginTop: '7em' }}>
-      <h1>Home page</h1>
-      <h3>Go to <Link to='/activities'>Activities</Link></h3>
-    </Container>
+    <Segment inverted textAlign='center' vertical className='masthead'>
+      <Container text>
+        <Header as='h1' inverted>
+          <Image
+            size='massive'
+            src='/assets/logo.png'
+            alt='logo'
+            style={{ marginBottom: 12 }}
+          />
+          Social-me-die
+        </Header>
+        <Header as='h2' inverted content='Welcome to my little social media experiment' />
+        <Button as={Link} to='/activities' size='huge' inverted>
+          Take me to the activities!
+        </Button>
+      </Container>
+    </Segment>
   )
 }
 
