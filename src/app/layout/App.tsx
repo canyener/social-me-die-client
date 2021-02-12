5983f7cd3f8d2b404b2684125025dc71d21c175c
canyener <canyener1337@gmail.com>
2021-02-12 23:15:37 +0300

    Adds route for not found page and wrapes Routes with Switch component

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react'
 import { Container } from 'semantic-ui-react'
 import { observer } from 'mobx-react-lite'
-import { Route, RouteComponentProps, withRouter } from 'react-router-dom'
+import { Route, RouteComponentProps, Switch, withRouter } from 'react-router-dom'
 
 import NavBar from '../../features/nav/NavBar'
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard'
@@ -21,14 +21,16 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
           <Fragment>
             <NavBar />
             <Container style={{ marginTop: '7em' }}>
-              <Route exact path='/activities' component={ActivityDashboard} />
-              <Route path='/activities/:id' component={ActivityDetails} />
-              <Route
-                key={location.key}
-                path={['/createActivity', '/manage/:id']}
-                component={ActivityForm}
-              />
-              <Route component={NotFound} />
+              <Switch>
+                <Route exact path='/activities' component={ActivityDashboard} />
+                <Route path='/activities/:id' component={ActivityDetails} />
+                <Route
+                  key={location.key}
+                  path={['/createActivity', '/manage/:id']}
+                  component={ActivityForm}
+                />
+                <Route component={NotFound} />
+              </Switch>
             </Container>
           </Fragment>
         )}
