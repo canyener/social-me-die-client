7b9970cdd837d6a5737365dda1d5f4fd967b97c2
canyener <canyener1337@gmail.com>
2021-03-07 22:41:56 +0300

    Add TextInput component

---
+++ src/app/common/form/TextInput.tsx
@@ -0,0 +1,25 @@
+import { FieldRenderProps } from 'react-final-form'
+import { Form, FormFieldProps, Label } from 'semantic-ui-react'
+
+interface IProps extends FieldRenderProps<string, HTMLInputElement>, FormFieldProps { }
+
+const TextInput: React.FC<IProps> = ({
+  input,
+  width,
+  type,
+  placeholder,
+  meta: { touched, error }
+}) => {
+  return (
+    <Form.Field error={touched && !!error} width={width}>
+      <input {...input} type={type} placeholder={placeholder} />
+      {
+        touched && error && (
+          <Label basic color='red'>{error}</Label>
+        )
+      }
+    </Form.Field>
+  )
+}
+
+export default TextInput
