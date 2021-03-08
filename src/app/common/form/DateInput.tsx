42c51da3e7c46e19df699441555849c21d011694
canyener <can.yener@hotmail.com.tr>
2021-03-08 17:58:50 +0300

    Add custom DateInput component

---
+++ src/app/common/form/DateInput.tsx
@@ -0,0 +1,35 @@
+import { FieldRenderProps } from 'react-final-form'
+import { Form, FormFieldProps, Label } from 'semantic-ui-react'
+import { DateTimePicker } from 'react-widgets'
+
+interface IProps
+  extends FieldRenderProps<any, HTMLInputElement>,
+  FormFieldProps {
+  id?: string | undefined
+}
+
+const DateInput: React.FC<IProps> = ({
+  input,
+  width,
+  placeholder,
+  meta: { touched, error },
+  ...rest
+}) => {
+  return (
+    <Form.Field error={touched && !!error} width={width}>
+      <DateTimePicker
+        placeholder={placeholder}
+        value={input.value || null}
+        onChange={input.onChange}
+        {...rest}
+      />
+      {
+        touched && error && (
+          <Label basic color='red'>{error}</Label>
+        )
+      }
+    </Form.Field>
+  )
+}
+
+export default DateInput
