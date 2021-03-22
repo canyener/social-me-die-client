9c059432b69f2ac3eeb0230ad40f1e1f93715d5a
canyener <can.yener@hotmail.com.tr>
2021-03-22 11:05:11 +0300

    Remove non-null assertion operator from activity date prop

--- src/features/activities/dashboard/ActivityListItem.tsx
+++ src/features/activities/dashboard/ActivityListItem.tsx
@@ -22,7 +22,7 @@ const ActivityListItem: React.FC<{ activity: IActivity }> = ({ activity }) => {
         </Item.Group>
       </Segment>
       <Segment>
-        <Icon name='clock' /> {format(activity.date!, 'h:mm a')}
+        <Icon name='clock' /> {format(activity.date, 'h:mm a')}
         <Icon name='marker' /> {activity.venue}, {activity.city}
       </Segment>
       <Segment secondary>
