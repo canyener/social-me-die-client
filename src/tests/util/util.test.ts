7add4ef908a6946a539c48d6319154afa9b4b42a
canyener <can.yener@hotmail.com.tr>
2021-03-22 10:59:28 +0300

    Add test cases for combineDateAndTime function

---
+++ src/tests/util/util.test.ts
@@ -0,0 +1,30 @@
+import { combineDateAndTime } from "../../app/common/util/util"
+import { register } from 'timezone-mock'
+
+describe('#combineDateAndTime', () => {
+
+  test('Should return correct combined date', () => {
+
+    const date = new Date(2021, 2, 11, 12, 12)
+    const time = new Date(2021, 11, 24, 11, 11)
+
+    const result = combineDateAndTime(date, time)
+
+    const dateText = result.toLocaleDateString('tr-TR', { timeZone: 'Europe/Istanbul' })
+    const timeText = result.toLocaleTimeString('tr-TR', { timeZone: 'Europe/Istanbul' })
+
+    const actual = `${dateText} ${timeText}`
+
+    expect(actual).toBe('11.03.2021 11:11:00')
+  })
+
+  test('Should return type of Date', () => {
+
+    const date = new Date(2021, 2, 11, 12, 12)
+    const time = new Date(2021, 11, 24, 11, 11)
+
+    const result = combineDateAndTime(date, time)
+
+    expect(result instanceof Date).toBe(true)
+  })
+})
