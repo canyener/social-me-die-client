e151dbffc7092600838b102f555f95f0e63d9d42
canyener <canyener1337@gmail.com>
2021-03-28 22:44:04 +0300

    Refactor test case to cover month and day formats when they're less than 10. (E.x.: should have 02 instead of 2)

--- src/tests/util/util.test.ts
+++ src/tests/util/util.test.ts
@@ -2,17 +2,20 @@ import { combineDateAndTime } from "../../app/common/util/util"
 
 describe('#combineDateAndTime', () => {
 
-  test('Should return correct combined date', () => {
+  test('Should return correct combined date in ISO format', () => {
 
-    const date = new Date(2020, 9, 10, 9, 9)
-    const time = new Date(2000, 1, 2, 12, 12);
+    const date = new Date(2020, 1, 2, 9, 9)
+    const time = new Date(2000, 11, 11, 12, 12);
 
     const result = combineDateAndTime(date, time)
+    const expected = '2020-02-02T12:12:00'
 
-    const expected = '2020-10-10T12:12:00'
-    const actual = `${result.getFullYear()}-${result.getMonth() + 1}-${result.getDate()}T${result.getHours()}:${result.getMinutes()}:00`
+    const actualDate = result.toISOString().split('T')[0]
+    const actualTime = `${result.getHours()}:${result.getMinutes()}:00`
+    
+    const actualCombinedDate = `${actualDate}T${actualTime}`
 
-    expect(actual).toBe(expected)
+    expect(actualCombinedDate).toBe(expected)
   })
 
   test('Should return type of Date', () => {
